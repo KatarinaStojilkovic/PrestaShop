a7f4558cb1c85c345f2cb18b61788c5ebe95e861
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-19 14:59:09 +0200

    "Removed uneccessary pages"

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,14 +1,13 @@
 import {test, expect} from '@playwright/test'
 import {NavigationPage} from '../page-objects/navigationPage'
 import { PageManager } from '../page-objects/pageManager'
-import {signInPage} from '../page-objects/signInPage'
-import {signUpPage} from '../page-objects/signUpPage'
-import {artPage} from '../page-objects/artPage'
-import {accessoriesPage} from '../page-objects/accessoriesPage'
-import {clothesPage} from '../page-objects/clothesPage'
 
 
- test.beforeEach(async({page}) =>{
-await page.goto('https://demo.prestashop.com/#/en/front')
+test.only('has title', async ({page}) => {
+    await page.goto('https://demo.prestashop.com/#/en/front');
 })
+
+//  test.beforeEach(async({page}) =>{
+// await page.goto('https://demo.prestashop.com/#/en/front')
+// })
 
