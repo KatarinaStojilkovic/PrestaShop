3f680124ede25359074ec76ddb7a9f9126338d5f
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-19 15:02:56 +0200

    "Changed some codes and configuration"

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -7,7 +7,7 @@ test.only('has title', async ({page}) => {
     await page.goto('https://demo.prestashop.com/#/en/front');
 })
 
-//  test.beforeEach(async({page}) =>{
-// await page.goto('https://demo.prestashop.com/#/en/front')
-// })
+ test.beforeEach(async({page}) =>{
+await page.goto('https://demo.prestashop.com/#/en/front')
+})
 
