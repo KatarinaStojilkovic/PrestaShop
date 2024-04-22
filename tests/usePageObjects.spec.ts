2fcb4462366fc27459f317e5f629f8489c9936dd
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-22 10:08:47 +0200

    "Changed the structure of codes, to look nice."

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -3,28 +3,28 @@ import {NavigationPage} from '../page-objects/navigationPage'
 import { PageManager } from '../page-objects/pageManager'
 
 test.beforeEach(async({page}) =>{
-await page.goto('https://demo.prestashop.com/#/en/front')
+    await page.goto('https://demo.prestashop.com/#/en/front')
 })
 
 // test('has title', async ({page}) => {
-//     await page.goto('https://demo.prestashop.com/#/en/front');
+//  await page.goto('https://demo.prestashop.com/#/en/front');
 // })
 
 
 
 
 
-// test ('navigate to form page', async({page}) => {
+//test ('navigate to form page', async({page}) => {
 
-//     const navigateTo = new NavigationPage(page)
-//     await navigateTo.signInPage()
+//  const navigateTo = new NavigationPage(page)
+//  await navigateTo.signInPage()
 // })
 
 
 test ('Language Change', async ({page}) => {
-await page.waitForTimeout(5000)
-const frame = await page.frameLocator('#framelive').getByLabel('Language dropdown').first().click()
-const lang = await page.frameLocator('#framelive').getByRole('link', {name: 'Bosanski'}).click()
+    await page.waitForTimeout(5000)
+    const frame = await page.frameLocator('#framelive').getByLabel('Language dropdown').first().click()
+    const lang = await page.frameLocator('#framelive').getByRole('link', {name: 'Bosanski'}).click()
 
 })
 
@@ -33,5 +33,6 @@ test ('Sign In', async({page}) => {
     await page.waitForTimeout(5000)
     //await page.getByRole('link', {name: 'Sign in'}).click()
     const frame = await page.frameLocator('#framelive').getByRole('link', {name: 'Sign in'}).first().click()
+
     })
 
