dee0dd8303a442d083ca590c11b5af8eebcb7557
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-24 15:28:39 +0200

    "Changed the default timeout"

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   expect: {
-    timeout:1000,
+    timeout:20000,
   },
   testDir: './tests',
   /* Run tests in files in parallel */
