5932f997777808394ba0c7d3363bacb2e6e89f47
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-24 10:38:31 +0200

    "Formatted the PageManager page"

--- page-objects/pageManager.ts
+++ page-objects/pageManager.ts
@@ -1,25 +1,17 @@
     import {Page, expect} from '@playwright/test';
     import {signUpPage} from '../page-objects/signUpPage'
 
-
     export class PageManager {
 
     private readonly page: Page
     private readonly signUpPage: signUpPage
-    
-
-
 
     constructor (page: Page) {
     this.page = page
     this.signUpPage = new signUpPage(this.page)
-
-
     }
-
-
+    
     onSignUpPage () {
-        return this.signUpPage
+    return this.signUpPage
     }
-
     }
