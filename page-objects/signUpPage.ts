e19689c802093b5a2615f2a12a437e25cf1c20f2
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-24 13:01:29 +0200

    "Formatted documents"

--- page-objects/signUpPage.ts
+++ page-objects/signUpPage.ts
@@ -1,77 +1,76 @@
-    import { Locator, Page } from "@playwright/test";
-    import { DateModule, SimpleDateModule, faker } from '@faker-js/faker';
+import { Locator, Page } from "@playwright/test";
+import { DateModule, SimpleDateModule, faker } from '@faker-js/faker';
 
-    export class signUpPage {
+export class signUpPage {
 
     readonly page: Page
-                
-    constructor (page: Page){
-    this.page = page
+
+    constructor(page: Page) {
+        this.page = page
     }
-                
-    async createUser (){
-            
-    // Creating locators in order to make a user input of the valid info
-    // Using Faker-js library to create fake name and email in order to sign up for the website
-            
-    await this.page.frameLocator('#framelive').getByRole('link', {name: 'Sign in'}).first().click()
-    await this.page.frameLocator('#framelive').getByRole('link', {name: 'No account? Create one here'}).click()
-    await this.page.frameLocator('#framelive').getByText('Mrs.').first().click()
-    await this.page.frameLocator('#framelive').getByLabel('First name').fill(faker.person.firstName())
-    await this.page.frameLocator('#framelive').getByLabel('Last name').fill(faker.person.lastName())
-    await this.page.frameLocator('#framelive').getByLabel('Email').fill(faker.internet.email())
-    await this.page.frameLocator('#framelive').getByLabel('Password input').fill(faker.internet.password())
-    await this.page.frameLocator('#framelive').getByPlaceholder('MM/DD/YYYY').fill( faker.date.between('1950/01/01', '2024/05/05').toLocaleDateString('en-US'))
-    await this.page.frameLocator('#framelive').getByText('I agree to the terms and conditions and the privacy policy').click()
-    await this.page.frameLocator('#framelive').getByText('Customer data privacy').click()
-    await this.page.frameLocator('#framelive').getByRole('button', {name: 'Save'}).click() 
+
+    async createUser() {
+
+        // Creating locators in order to make a user input of the valid info
+        // Using Faker-js library to create fake name and email in order to sign up for the website
+
+        await this.page.frameLocator('#framelive').getByRole('link', { name: 'Sign in' }).first().click()
+        await this.page.frameLocator('#framelive').getByRole('link', { name: 'No account? Create one here' }).click()
+        await this.page.frameLocator('#framelive').getByText('Mrs.').first().click()
+        await this.page.frameLocator('#framelive').getByLabel('First name').fill(faker.person.firstName())
+        await this.page.frameLocator('#framelive').getByLabel('Last name').fill(faker.person.lastName())
+        await this.page.frameLocator('#framelive').getByLabel('Email').fill(faker.internet.email())
+        await this.page.frameLocator('#framelive').getByLabel('Password input').fill(faker.internet.password())
+        await this.page.frameLocator('#framelive').getByPlaceholder('MM/DD/YYYY').fill(faker.date.between('1950/01/01', '2024/05/05').toLocaleDateString('en-US'))
+        await this.page.frameLocator('#framelive').getByText('I agree to the terms and conditions and the privacy policy').click()
+        await this.page.frameLocator('#framelive').getByText('Customer data privacy').click()
+        await this.page.frameLocator('#framelive').getByRole('button', { name: 'Save' }).click()
 
     }
     // Creating the locators for the assertions that will be called on test page
-                    
-    async getSignInButton () {    
-    return this.page.frameLocator('#framelive').getByRole('link', { name: 'Sign in' }).first()
+
+    async getSignInButton() {
+        return this.page.frameLocator('#framelive').getByRole('link', { name: 'Sign in' }).first()
     }
 
-    async getSignUpButton (){
-    return this.page.frameLocator('#framelive').getByRole('link', {name: 'No account? Create one here'})
+    async getSignUpButton() {
+        return this.page.frameLocator('#framelive').getByRole('link', { name: 'No account? Create one here' })
     }
 
-    async getRadioButton (){
-    return this.page.frameLocator('#framelive').getByText('Mrs.').first()
-    }
-                
-    async getFirstName (){
-    return this.page.frameLocator('#framelive').getByLabel('First name')
+    async getRadioButton() {
+        return this.page.frameLocator('#framelive').getByText('Mrs.').first()
     }
 
-    async getLastName (){
-    return this.page.frameLocator('#framelive').getByLabel('Last name')
+    async getFirstName() {
+        return this.page.frameLocator('#framelive').getByLabel('First name')
     }
 
-    async getEmail (){
-    return this.page.frameLocator('#framelive').getByLabel('Email')
+    async getLastName() {
+        return this.page.frameLocator('#framelive').getByLabel('Last name')
     }
 
-    async getPassword (){
-    return this.page.frameLocator('#framelive').getByLabel('Password input')
+    async getEmail() {
+        return this.page.frameLocator('#framelive').getByLabel('Email')
     }
 
-    async getBirthday (){
-    return this.page.frameLocator('#framelive').getByPlaceholder('MM/DD/YYYY')
+    async getPassword() {
+        return this.page.frameLocator('#framelive').getByLabel('Password input')
     }
 
-    async getCheckBox (){
-    return this.page.frameLocator('#framelive').getByText('I agree to the terms and conditions and the privacy policy')
+    async getBirthday() {
+        return this.page.frameLocator('#framelive').getByPlaceholder('MM/DD/YYYY')
     }
 
-    async getCheckBox1 (){
-    return this.page.frameLocator('#framelive').getByText('Customer data privacy')
+    async getCheckBox() {
+        return this.page.frameLocator('#framelive').getByText('I agree to the terms and conditions and the privacy policy')
     }
 
-    async getSaveButton (){
-    return this.page.frameLocator('#framelive').getByRole('button', {name: 'Save'})
+    async getCheckBox1() {
+        return this.page.frameLocator('#framelive').getByText('Customer data privacy')
     }
 
+    async getSaveButton() {
+        return this.page.frameLocator('#framelive').getByRole('button', { name: 'Save' })
     }
-                
+
+}
--- tests/signUpTest.spec.ts
+++ tests/signUpTest.spec.ts
@@ -1,82 +1,82 @@
-    import {test, expect} from '@playwright/test'
-    import { PageManager } from '../page-objects/pageManager'
-    import { faker } from '@faker-js/faker';
+import { test, expect } from '@playwright/test'
+import { PageManager } from '../page-objects/pageManager'
+import { faker } from '@faker-js/faker';
 
-    // The page has to open everytime before starting the test since nothing is saved when the page gets reloaded or it takes some time to load
-    test.beforeEach(async({page}) =>{
-        await page.goto('https://demo.prestashop.com/#/en/front')
-    })
+// The page has to open everytime before starting the test since nothing is saved when the page gets reloaded or it takes some time to load
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://demo.prestashop.com/#/en/front')
+})
 
-    test('User can register with all valid credentials', async({page}) => {
+test('User can register with all valid credentials', async ({ page }) => {
 
-        // Creating a const in order to call up the locators easier and assert the user inputs
-        const pm = new PageManager(page)
-            
-        // Sign in page assertion
-        const signIn = await pm.onSignUpPage().getSignInButton()
-        await signIn.click()
-        expect(signIn).toBeDefined()
-        
-        // Sing up button assertion
-        const signUpButton = await pm.onSignUpPage().getSignUpButton()
-        await signUpButton.click()
-    
-        // Radio Button assertion
-        const radioButton = await pm.onSignUpPage().getRadioButton()
-        await radioButton.click()
-        const isChecked = await radioButton.isChecked()
-        expect(isChecked).toBeTruthy()
+    // Creating a const in order to call up the locators easier and assert the user inputs
+    const pm = new PageManager(page)
 
-        // First Name assertion
-        const firstName = await pm.onSignUpPage().getFirstName()
-        const generatedFirstName = faker.person.firstName()
-        await firstName.fill(generatedFirstName)
-        const fieldValue = await firstName.inputValue()
-        expect(fieldValue).toBe(generatedFirstName)
+    // Sign in page assertion
+    const signIn = await pm.onSignUpPage().getSignInButton()
+    await signIn.click()
+    expect(signIn).toBeDefined()
 
-        // Last Name assertion
-        const lastName = await pm.onSignUpPage().getLastName()
-        const generatedLastName = faker.person.lastName() 
-        await lastName.fill(generatedLastName)
-        const fieldValue1 = await lastName.inputValue()
-        expect(fieldValue1).toBe(generatedLastName)
+    // Sing up button assertion
+    const signUpButton = await pm.onSignUpPage().getSignUpButton()
+    await signUpButton.click()
 
-        // Email assertion
-        const email = await pm.onSignUpPage().getEmail()
-        const generatedEmail = faker.internet.email()
-        await email.fill(generatedEmail)
-        const emailInput = await email.inputValue()
-        expect(emailInput).toBe(generatedEmail)
+    // Radio Button assertion
+    const radioButton = await pm.onSignUpPage().getRadioButton()
+    await radioButton.click()
+    const isChecked = await radioButton.isChecked()
+    expect(isChecked).toBeTruthy()
 
-        // Password assertion
-        const password = await pm.onSignUpPage().getPassword()
-        const generatedPassword = faker.internet.password()
-        await password.fill(generatedPassword)
-        const passwordInput = await password.inputValue()
-        expect(passwordInput).toBe(generatedPassword)
+    // First Name assertion
+    const firstName = await pm.onSignUpPage().getFirstName()
+    const generatedFirstName = faker.person.firstName()
+    await firstName.fill(generatedFirstName)
+    const fieldValue = await firstName.inputValue()
+    expect(fieldValue).toBe(generatedFirstName)
 
-        // Birthday assertion
-        const birthday = await pm.onSignUpPage().getBirthday()
-        const generatedBday = faker.date.between('1950/01/01', '2024/05/05').toLocaleDateString('en-US')
-        await birthday.fill(generatedBday)
-        const birthdayInput = await birthday.inputValue()
-        expect(birthdayInput).toBe(generatedBday)
+    // Last Name assertion
+    const lastName = await pm.onSignUpPage().getLastName()
+    const generatedLastName = faker.person.lastName()
+    await lastName.fill(generatedLastName)
+    const fieldValue1 = await lastName.inputValue()
+    expect(fieldValue1).toBe(generatedLastName)
 
-        // Checkbox assertion
-        const checkBox = await pm.onSignUpPage().getCheckBox()
-        await checkBox.click()
-        const isChecked1 = await checkBox.isChecked()
-        expect(isChecked1).toBeTruthy()
+    // Email assertion
+    const email = await pm.onSignUpPage().getEmail()
+    const generatedEmail = faker.internet.email()
+    await email.fill(generatedEmail)
+    const emailInput = await email.inputValue()
+    expect(emailInput).toBe(generatedEmail)
 
-        // Checkbox assertion
-        const checkBox1 = await pm.onSignUpPage().getCheckBox1()
-        await checkBox1.click()
-        const isChecked2 = await checkBox1.isChecked()
-        expect(isChecked2).toBeTruthy()
+    // Password assertion
+    const password = await pm.onSignUpPage().getPassword()
+    const generatedPassword = faker.internet.password()
+    await password.fill(generatedPassword)
+    const passwordInput = await password.inputValue()
+    expect(passwordInput).toBe(generatedPassword)
 
-        // Save button assertion
-        const saveButton = await pm.onSignUpPage().getSaveButton()
-        await saveButton.click()
-        expect(saveButton).toBeDefined()
+    // Birthday assertion
+    const birthday = await pm.onSignUpPage().getBirthday()
+    const generatedBday = faker.date.between('1950/01/01', '2024/05/05').toLocaleDateString('en-US')
+    await birthday.fill(generatedBday)
+    const birthdayInput = await birthday.inputValue()
+    expect(birthdayInput).toBe(generatedBday)
 
-    }) 
+    // Checkbox assertion
+    const checkBox = await pm.onSignUpPage().getCheckBox()
+    await checkBox.click()
+    const isChecked1 = await checkBox.isChecked()
+    expect(isChecked1).toBeTruthy()
+
+    // Checkbox assertion
+    const checkBox1 = await pm.onSignUpPage().getCheckBox1()
+    await checkBox1.click()
+    const isChecked2 = await checkBox1.isChecked()
+    expect(isChecked2).toBeTruthy()
+
+    // Save button assertion
+    const saveButton = await pm.onSignUpPage().getSaveButton()
+    await saveButton.click()
+    expect(saveButton).toBeDefined()
+
+}) 
