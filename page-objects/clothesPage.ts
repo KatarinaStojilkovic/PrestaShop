26b9a0d00febad3571673ed4fd0dde84c2c1e484
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-24 12:57:16 +0200

    "Edited some tests"

--- page-objects/clothesPage.ts
+++ page-objects/clothesPage.ts
@@ -10,13 +10,47 @@ export class clothesPage {
             this.page = page
         }
 
+    // Creating the locators for the pages and elements on the website
     async onClothesPage (){
         await this.page.frameLocator('#framelive').getByRole('link', {name: 'Clothes'}).first().click()
 
     }
-    // async scrollToShirt (){
-    // const scrollToElement = this.page.frameLocator('#framelive').getByRole('link', {name: 'Brown bear printed sweater'})
-    // await scrollToElement.scrollIntoViewIfNeeded();
-    // }
+
+    async getClothesPage (){
+        return this.page.frameLocator('#framelive').getByRole('link', {name: 'Clothes'}).first()
+    }
+
+    async getElementInput (){
+        return this.page.frameLocator('#framelive').getByRole('link', {name: 'Brown bear printed sweater'}).first().click()
+    }
+
+    async getInputData (){
+        return this.page.frameLocator('#framelive').locator('#quantity_wanted').fill('1200')
+    }
+
+    async getAddToCart (){
+        return this.page.frameLocator('#framelive').getByRole('button', {name: 'Add to cart'}).first().click()
+    }
+
+    async getProceedToCheckout (){
+        return this.page.frameLocator('#framelive').getByRole('link', {name: 'Proceed to checkout'}).first().click()
+    }
+
+    async getArrowClick (){
+        return this.page.frameLocator('#framelive').locator('.input-group-btn-vertical').getByRole('button').first().click()
+  
+    }
+
+    async getErrorMessage (){
+        return this.page.frameLocator('#framelive').getByRole('alert')
+    }
+
+    async getTextErrorMessage (){
+        return this.page.frameLocator('#framelive').getByText('The available purchase order quantity for this product is 1200.')
+    }
+
+    async getProceedToButton (){
+        return this.page.frameLocator('iframe[name="framelive"]').locator('#main div').filter({ hasText: 'Proceed to checkout' }).nth(4)
+    }
 
 }
--- tests/clothesOrder.spec.ts
+++ tests/clothesOrder.spec.ts
@@ -13,21 +13,43 @@ test.beforeEach(async({page}) =>{
 
 test ('Clothes Page', async({page})=> {
 
+    const pm = new PageManager (page)
+    pm.onClothesPage().getClothesPage()
+
     //Asserting the Clothes page and clicking on the clothes page element
-    const onClothesPage = page.frameLocator('#framelive').getByRole('link', {name: 'Clothes'}).first()
-    await onClothesPage.click()
-    await onClothesPage.textContent()
-    await expect(onClothesPage).toContainText('Clothes')
-
-    // Scrolling to the element and inputing 
-    const scrollToElement = await page.frameLocator('#framelive').getByRole('link', {name: 'Brown bear printed sweater'}).first().click()
-    const quantity = await page.frameLocator('#framelive').locator('#quantity_wanted').fill('1200')
     
-    const addToCart = await page.frameLocator('#framelive').getByRole('button', {name: 'Add to cart'}).first().click()
-    const proceedToCheckout = await page.frameLocator('#framelive').getByRole('link', {name: 'Proceed to checkout'}).first().click()
+    const clothesPage = await pm.onClothesPage().getClothesPage()
+    await clothesPage.click()
+    await clothesPage.textContent()
+    await expect(clothesPage).toContainText('Clothes')
 
-    //const arrowUp = await page.locator('#framelive').locator('#class="btn btn-touchspin js-touchspin js-increase-product-quantity bootstrap-touchspin-up"').getByRole('button', {name: 'btn btn-touchspin js-touchspin js-increase-product-quantity bootstrap-touchspin-up'}).first().click()
+    // Scrolling to the element 
+    const scrollToElement = await pm.onClothesPage().getElementInput()
+
+    // Input of the number in field
+    const quantity = await pm.onClothesPage().getInputData()
+    
+    // Adding items to cart
+    const addToCart = await pm.onClothesPage().getAddToCart()
+
+    // Proceeding to Checkout
+    const proceedToCheckout = await pm.onClothesPage().getProceedToCheckout()
+
+    // Clicking the arrowUp
+    const arrowUp = await pm.onClothesPage().getArrowClick()
   
+    // Assertion of the Error Alert
+    const errorMessage = await pm.onClothesPage().getErrorMessage()
+    await expect(errorMessage).toBeVisible()
+
+    // Assertion of the Text Error Message saying that the product allows no more than 1200 products 
+    const textErrorMessage = await pm.onClothesPage().getTextErrorMessage()
+    await expect(textErrorMessage).toBeVisible()
+  
+    // Assertion of the Proceed To Checkout Button when it is not visible
+    const proceedToCheckoutButton = await pm.onClothesPage().getProceedToButton()
+    await expect(proceedToCheckoutButton).toBeVisible()
+
     
 }
 )
