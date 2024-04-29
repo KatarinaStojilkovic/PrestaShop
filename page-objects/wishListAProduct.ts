cab037d229ed939c862a18f4997e389a8bdab75a
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-29 12:37:17 +0200

    "Changed the asserts of the tests"

--- page-objects/wishListAProduct.ts
+++ page-objects/wishListAProduct.ts
@@ -6,7 +6,7 @@ export class wishListAProduct {
         this.page = page
     }
     // Finding locators for the product wishlisting and the heart button on the product's page
-    async chooseAnyProduct() {
+    async chooseASweaterProduct() {
         return this.page.frameLocator('#framelive').getByRole('link', { name: 'Today is a good day Framed poster' }).first().click()
     }
     async clickOnAHeartButton() {
@@ -15,22 +15,23 @@ export class wishListAProduct {
     async clickPopUpForSignIn() {
         return this.page.frameLocator('#framelive').getByRole('dialog').getByRole('link', { name: 'Sign in' }).first().click()
     }
-    async createNewWishlistPopUp() {
+    async ClickOnCreateNewWishlistPopUp() {
         return this.page.frameLocator('#framelive').getByText('add_circle_outline Create new').click()
     }
-    async addToWishlistProduct() {
+    async clickOnAddToWishlistProduct() {
         return this.page.frameLocator('#framelive').locator('#add-to-cart-or-refresh').getByRole('button', { name: 'favorite_border' }).click()
     }
     async nameNewWishlist() {
         await this.page.frameLocator('#framelive').getByPlaceholder('Add name').fill('New Wishlist')
     }
-    async createNewWishlist() {
+    async clickOnCreateNewWishlist() {
         return this.page.frameLocator('#framelive').getByRole('button', { name: 'Create wishlist' }).click()
     }
-    async newListCreated() {
-        return this.page.frameLocator('#framelive').getByText('New Wishlist').click()
+    async newListIsCreated() {
+        return this.page.frameLocator('#framelive').getByText('New Wishlist')
     }
     async productAddedToast() {
         return this.page.frameLocator('#framelive').getByText('Product added')
     }
+
 }
--- tests/wishlistAProduct.spec.ts
+++ tests/wishlistAProduct.spec.ts
@@ -10,7 +10,7 @@ test('User can wishlist a product when signed in', async ({ page }) => {
     // Creating a const in order to call up the locators easier and assert the user inputs
     const pm = new PageManager(page);
     // Going to the Wishlist page
-    await pm.onWishlistPage().chooseAnyProduct()
+    await pm.onWishlistPage().chooseASweaterProduct()
     await pm.onWishlistPage().clickOnAHeartButton()
     await pm.onWishlistPage().clickPopUpForSignIn()
     // Sign in page assertion
@@ -19,52 +19,18 @@ test('User can wishlist a product when signed in', async ({ page }) => {
     await pm.onSignUpPage().getSignUpButton()
     // Radio Button 
     await pm.onSignUpPage().getRadioButton()
-    // First Name assertion
-    const firstName = await pm.onSignUpPage().getFirstName()
-    const generatedFirstName = faker.person.firstName()
-    await firstName.fill(generatedFirstName)
-    const fieldValue = await firstName.inputValue()
-    expect(fieldValue).toBe(generatedFirstName)
-    // Last Name assertion
-    const lastName = await pm.onSignUpPage().getLastName()
-    const generatedLastName = faker.person.lastName()
-    await lastName.fill(generatedLastName)
-    const fieldValue1 = await lastName.inputValue()
-    expect(fieldValue1).toBe(generatedLastName)
-    // Email assertion
-    const email = await pm.onSignUpPage().getEmail()
-    const generatedEmail = faker.internet.email()
-    await email.fill(generatedEmail)
-    const emailInput = await email.inputValue()
-    expect(emailInput).toBe(generatedEmail)
-    // Password assertion
-    const password = await pm.onSignUpPage().getPassword()
-    const generatedPassword = faker.internet.password()
-    await password.fill(generatedPassword)
-    const passwordInput = await password.inputValue()
-    expect(passwordInput).toBe(generatedPassword)
-    // Birthday 
-    const birthday = await pm.onSignUpPage().getBirthday()
-    const generatedBday = faker.date.between('1950/01/01', '2024/05/05').toLocaleDateString('en-US')
-    await birthday.fill(generatedBday)
-    const birthdayInput = await birthday.inputValue()
-    expect(birthdayInput).toBe(generatedBday)
-    // Checkbox 
-    await (await pm.onSignUpPage().getCheckBox()).check()
-    await (await pm.onSignUpPage().getCheckBox1()).check()
-    // Save button 
-    await pm.onSignUpPage().getSaveButton()
+    await pm.onSignUpPage().createUser()
     // Creating a new Wishlist and asserting the created list
-    await pm.onWishlistPage().chooseAnyProduct()
-    await pm.onWishlistPage().addToWishlistProduct()
-    await pm.onWishlistPage().createNewWishlistPopUp()
-    // Assertion of the creating a new wishlist
-    expect('Create wishlist').toContain('Create wishlist')
+    await pm.onWishlistPage().chooseASweaterProduct()
+    await pm.onWishlistPage().clickOnAddToWishlistProduct()
+    // Creating a new wishlist
+    await pm.onWishlistPage().ClickOnCreateNewWishlistPopUp()
     await pm.onWishlistPage().nameNewWishlist()
-    await pm.onWishlistPage().createNewWishlist()
-    await pm.onWishlistPage().newListCreated()
+    await pm.onWishlistPage().clickOnCreateNewWishlist()
     // Asserton of the new wishlist
-    expect('New Wishlist').toContain('New Wishlist')
+    const newWishListCreation = await pm.onWishlistPage().newListIsCreated()
+    await newWishListCreation.click()
+    await expect(newWishListCreation).toHaveText('New Wishlist')
     // Assertion of the added product
     const alertDialog = await pm.onWishlistPage().productAddedToast()
     expect(alertDialog).toBeVisible
