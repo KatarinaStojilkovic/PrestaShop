519d77f669e608ee83d2b0810bd57b08b98b0390
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-30 08:43:26 +0200

    "Added new assertatitons and changed the test"

--- page-objects/cartProductsCalculation.ts
+++ page-objects/cartProductsCalculation.ts
@@ -10,11 +10,12 @@ export class cartProductsCalculation {
     async proceedToCheckoutButton() {
         await this.page.frameLocator('#framelive').getByRole('link', { name: 'Proceed to checkout' }).first().click()
     }
-    async sweaterPriceDiscsount() {
-        return this.page.frameLocator('#framelive').getByText('€34.46')
+    async sweaterPriceDiscount() {
+        return this.page.frameLocator('#framelive').locator('.current-price-value').innerText()
     }
-    async increaseTheProductNumber() {
-        for (let i = 0; i < 4; i++) {
+    // Dynamic values in order to call up the function
+    async increaseTheProductNumber(counter: number) {
+        for (let i = 0; i < counter; i++) {
             await this.page.frameLocator('#framelive').locator('.input-group-btn-vertical').getByRole('button').first().click()
         }
     }
@@ -22,6 +23,7 @@ export class cartProductsCalculation {
         return this.page.frameLocator('#framelive').getByLabel('Hummingbird printed sweater')
     }
     async priceMultipliedBy5() {
-        return this.page.frameLocator('#framelive').locator('#cart-subtotal-products').getByText('€')
+        return this.page.frameLocator('#framelive').getByRole('listitem').locator('span').filter({ hasText: '€172.32' }).innerText()
     }
+    //cena*5 = nova cena; asertacija!
 }
--- tests/calculateProductsPage.spec.ts
+++ tests/calculateProductsPage.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import { assert } from 'console'
 
 // The page has to open everytime before starting the test since nothing is saved when the page gets reloaded or it takes some time to load
 test.beforeEach(async ({ page }) => {
@@ -11,16 +12,19 @@ test('Total calculation of the products in the shopping cart', async ({ page }) 
     await pm.onClothesPage().clickOnClothesMenu()
     await pm.onClothesPage().hummingbirdPrintedSweater()
     // Assertion of the price per 1 Hummingbird Sweater
-    const productPrice = await pm.onCartProductsPage().sweaterPriceDiscsount()
-    expect(productPrice).toHaveText('€34.46')
+    const productPrice = await pm.onCartProductsPage().sweaterPriceDiscount()
+    // Console.log to check the product price per 1 item
+    console.log('price read', productPrice)
     await pm.onClothesPage().clickOnAddToCartButton()
     await pm.onCartProductsPage().proceedToCheckoutButton()
-    await pm.onCartProductsPage().increaseTheProductNumber()
+    await pm.onCartProductsPage().increaseTheProductNumber(4)
     // Assertion of the quantity
     const inputOfTheSweaters = await pm.onCartProductsPage().assertQuantity()
     await expect(inputOfTheSweaters).toHaveValue('5')
-    // Assertion of the multiplied price
     const priceMultipliedBy5 = await pm.onCartProductsPage().priceMultipliedBy5()
-    await expect(priceMultipliedBy5).toBeVisible()
+    // Console.log to check the product price per more items
+    console.log('new price', priceMultipliedBy5)
+    // Assertion that the product price and the new price are not equal
+    expect(productPrice).not.toEqual(priceMultipliedBy5)
 })
 
