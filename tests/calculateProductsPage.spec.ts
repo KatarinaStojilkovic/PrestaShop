34b78e2f804303c9425163dc286f1b98dea61b71
Katarina Stojilkovic <katarina.stojilkovic5@gmail.com>
2024-04-30 09:41:15 +0200

    "New assertions"

--- tests/calculateProductsPage.spec.ts
+++ tests/calculateProductsPage.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
 import { assert } from 'console'
+import { parse } from 'path'
 
 // The page has to open everytime before starting the test since nothing is saved when the page gets reloaded or it takes some time to load
 test.beforeEach(async ({ page }) => {
@@ -15,6 +16,9 @@ test('Total calculation of the products in the shopping cart', async ({ page }) 
     const productPrice = await pm.onCartProductsPage().sweaterPriceDiscount()
     // Console.log to check the product price per 1 item
     console.log('price read', productPrice)
+    const parseProductPrice = parseFloat(productPrice)
+    const priceMultipliedByFive = parseProductPrice *5
+    console.log('Parsed value', priceMultipliedByFive)
     await pm.onClothesPage().clickOnAddToCartButton()
     await pm.onCartProductsPage().proceedToCheckoutButton()
     await pm.onCartProductsPage().increaseTheProductNumber(4)
@@ -22,9 +26,11 @@ test('Total calculation of the products in the shopping cart', async ({ page }) 
     const inputOfTheSweaters = await pm.onCartProductsPage().assertQuantity()
     await expect(inputOfTheSweaters).toHaveValue('5')
     const priceMultipliedBy5 = await pm.onCartProductsPage().priceMultipliedBy5()
+    const parseProductPriceMultiplied = parseFloat(priceMultipliedBy5)
+    console.log('Product price parsed and multiplied by 5', parseProductPriceMultiplied)
     // Console.log to check the product price per more items
     console.log('new price', priceMultipliedBy5)
     // Assertion that the product price and the new price are not equal
-    expect(productPrice).not.toEqual(priceMultipliedBy5)
+    expect(productPrice).toEqual(priceMultipliedBy5)
 })
 
